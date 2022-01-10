0042a280558fd3f3a5a4da7860095c34fdc29331
root <root@DESKTOP-E4TOMJ2.localdomain>
2022-01-10 10:45:05 -0300

    Branch Principal

--- bank-loan-api/src/main/java/br/com/fabio/bankloanapi/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fabio.bankloanapi.repositories;
-
-import br.com.fabio.bankloanapi.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
--- bank-loan-api/src/main/java/br/com/fabio/bankloanapi/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fabio.bankloanapi.repositories;
-
-import br.com.fabio.bankloanapi.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
